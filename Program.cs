1d5764a6f5507ce14bb7c90bf5f9c3b935959364
Henrik Kjellander <henrik.kjellander@unity3d.com>
2020-03-13 12:31:06 +0100

    Skeleton project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChangelogValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
